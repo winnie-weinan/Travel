8292cc59a9d2f2478a913e753db363024a291548
winnie-weinan <weinan_yx@163.com>
2019-07-21 00:15:06 +0800

    project header init

--- src/main.js
+++ src/main.js
@@ -4,8 +4,13 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastClick from 'fastclick'
-import './assets/styles/reset.css'
-import './assets/styles/border.css'
+// import './assets/styles/reset.css'
+// import './assets/styles/border.css'
+// import './assets/styles/iconfont.css'
+// build/webpack.base.con.js中第34行设置简略文件路径别名：styles
+import 'styles/reset.css'
+import 'styles/border.css'
+import 'styles/iconfont.css'
 
 Vue.config.productionTip = false
 fastClick.attach(document.body)
--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,15 +1,20 @@
 <template>
   <div>
-     hello world
+    <home-header></home-header>
   </div>
 </template>
 
 <script>
+import HomeHeader from './components/Header'
+
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: {
+    HomeHeader
+  }
 }
 </script>
 
-<style >
+<style scoped>
 
 </style>

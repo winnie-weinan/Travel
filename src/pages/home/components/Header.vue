3c8b9e96612d972cf9978e7ac551cf30b599ef01
winnie-weinan <weinan_yx@163.com>
2019-07-21 15:01:53 +0800

    轮播图

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -5,7 +5,7 @@
     </div>
     <div class="header-input">
       <span class="iconfont">&#xe632;</span>
-      输入城市/景点/游玩主题/测试
+      输入城市/景点/游玩主题
     </div>
     <div class="header-right">
       城市
@@ -13,7 +13,6 @@
     </div>
   </div>
 </template>
-
 
 <script>
 export default {
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,55 @@
+<template>
+<!--撑开高度解决抖动-->
+<div class="wrapper">
+  <swiper :options="swiperOption" >
+    <!-- slides -->
+    <swiper-slide v-for="item of swiperList" :key="item.id">
+      <img class="swiper-img" :src="item.imgUrl" alt="I'm Slide 1">
+    </swiper-slide>
+    <!-- Optional controls -->
+    <div class="swiper-pagination"  slot="pagination"></div>
+    <!--轮播左右箭头不需要删掉-->
+    <!--<div class="swiper-button-prev" slot="button-prev"></div>-->
+    <!--<div class="swiper-button-next" slot="button-next"></div>-->
+    <!--背景灰色小条去掉-->
+    <!--<div class="swiper-scrollbar"   slot="scrollbar"></div>-->
+  </swiper>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1609/47/0cfdc871ac183702.jpg_750x200_f6896acd.jpg'
+      }, {
+        id: '0002',
+        imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20197/f4168ba20e0f5218ed83e743efd802b2.jpg_750x200_53edd3a0.jpg'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  /*scoped导致单独写样式无效，swiper组件去做显示的，所以使用样式穿透>>>*/
+  .wrapper>>>.swiper-pagination-bullet-active
+    background: #fff !important
+  .wrapper
+    overflow: hidden
+    width: 100%
+    height: 0
+    /*图片宽高比：640/200=31.25，宽高比例始终保持在31.25%,高度相对于100%的宽度自动撑开31.25%*/
+    padding-bottom: 31.25%
+    background: #eee
+    .swiper-img
+      width :100%
+
+</style>

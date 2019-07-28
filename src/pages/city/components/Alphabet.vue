9cd178452db8bd21a51194d6d096ce721ba6e5cd
winnie-weinan <weinan_yx@163.com>
2019-07-28 10:29:13 +0800

    搜索功能的实现 city-search-logic

--- src/pages/city/components/Alphabet.vue
+++ src/pages/city/components/Alphabet.vue
@@ -79,7 +79,7 @@ export default {
           clearTimeout(this.timer)
         }
         this.timer = setTimeout(() => {
-          const touchY = e.touches[0].clientY - 79
+          const touchY = e.touches[0].clientY - 29
           const index = Math.floor((touchY - this.startY) / 20)
           if (index >= 0 && index < this.letters.length) {
             this.$emit('change', this.letters[index])

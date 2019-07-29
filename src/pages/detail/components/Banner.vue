85a18c3f431017109f6c299abe2b2b8d56c23260
winnie-weinan <weinan_yx@163.com>
2019-07-30 06:57:05 +0800

    detail的banner部分，包含img和info

---
+++ src/pages/detail/components/Banner.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="banner">
+    <img class="banner-img" src="//img1.qunarzz.com/sight/p0/1607/7c/7cda8b6782dabd80b4.img.jpg_600x330_8572a930.jpg" alt="">
+    <div class="banner-info">
+      <div class="banner-title">上海迪士尼乐园</div>
+      <div class="banner-number"><span class="iconfont banner-icon">&#xe889;</span>77</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DetailBanner'
+}
+</script>
+
+<style lang="stylus" scoped>
+  .banner
+    position: relative
+    overflow: hidden
+    height: 0
+    padding-bottom:55%
+    .banner-img
+      width: 100%
+    .banner-info
+      display: flex
+      position: absolute
+      left: 0
+      right: 0
+      bottom: 0
+      line-height: .6rem
+      color: #fff
+      background-image: linear-gradient(top, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.8))
+      .banner-title
+        flex: 1
+        font-size: .32rem
+        padding: 0 .2rem
+        text-align: left
+      .banner-number
+        margin-top: .14rem
+        padding: 0 .4rem
+        height: .32rem
+        line-height: .32rem
+        border-radius: .4rem
+        background: rgba(0, 0, 0, .8)
+        font-size: .24rem
+        .banner-icon
+          font-size: .24rem
+
+</style>

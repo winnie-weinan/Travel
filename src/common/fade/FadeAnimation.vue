886f10f31aedfcb596f77805bc99b2056af9650c
winnie-weinan <weinan_yx@163.com>
2019-08-01 00:38:08 +0800

    branch detail-animation 项目加入基础动画，画廊打开后渐隐渐现

---
+++ src/common/fade/FadeAnimation.vue
@@ -0,0 +1,20 @@
+<template>
+  <!--渐隐渐现动画效果-->
+  <transition>
+    <!--外部组件插入进来的插槽-->
+    <slot></slot>
+  </transition>
+</template>
+
+<script>
+export default {
+  name: 'FadeAnimation'
+}
+</script>
+
+<style lang="stylus" scoped>
+  .v-enter, .v-leave-to
+    opacity: 0
+  .v-enter-active, .v-leave-active
+    transition: opacity 1s
+</style>
--- src/pages/detail/components/Banner.vue
+++ src/pages/detail/components/Banner.vue
@@ -13,12 +13,15 @@
         </div>
       </div>
     </div>
-    <common-gallery :imgs="bannerImgs" v-show="showGallery" @close="handleGalleryClose"></common-gallery>
+    <fade-animation>
+      <common-gallery :imgs="bannerImgs" v-show="showGallery" @close="handleGalleryClose"></common-gallery>
+    </fade-animation>
   </div>
 </template>
 
 <script>
 import CommonGallery from 'common/gallery/Gallery'
+import FadeAnimation from 'common/fade/FadeAnimation'
 export default {
   name: 'DetailBanner',
   props: {
@@ -44,7 +47,8 @@ export default {
     }
   },
   components: {
-    CommonGallery
+    CommonGallery,
+    FadeAnimation
   }
 }
 </script>
